a0a2e53bad3e65cc3e3f656784e5ff06917abb72
Paola Uudiales <Al03107338@tecmilenio.mx>
2025-04-12 20:33:55 +0000

    Se realizó una segunda rama y se agregó una linea mas al achivo app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
 print("Conexión a Linux para hacer operaciones con git")
 print("Edición de archivo app.py desde la rama1")
 print("Hola esta es una nueva linea de código")
+
+print("Hola desde la rama 2")
