67b637e89d3802a685ea636bee48ceac8ca9ad91
Paola Uudiales <Al03107338@tecmilenio.mx>
2025-04-12 20:47:20 +0000

    Revert "Se agregarón 2 lineas al archivo"
    This reverts commit 35863e9bc2c38a77c7c43d24f8d685144ac51b2e.

--- app.py
+++ app.py
@@ -3,6 +3,3 @@ print("Edición de archivo app.py desde la rama1")
 print("Hola esta es una nueva linea de código")
 
 print("Hola desde la rama 2")
-
-print("HOLA ESTE ES UN MENSAJE DESDE MAIN")
-print("HOLA ESTE ES UN MENSAJE DESDE MAIN2")
